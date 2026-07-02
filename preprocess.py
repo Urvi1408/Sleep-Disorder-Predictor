b114f1d03a821f841bc1ace08280c20e6eae0f2f
Urvi1408 <winsome.urvi@gmail.com>
2026-07-02 20:13:35 +0530

    added preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+def preprocess():
+    df = pd.read_csv("sleep_disorder_data_advanced_2000 (1).csv")
+
+    df["Sleep_disorder"] = df["Sleep_disorder"].fillna(
+        df["Sleep_disorder"].mode()[0]
+    )
+
+    encoders = {}
+
+    categorical_columns = [
+        "Gender",
+        "Occupation",
+        "BMI_category"
+    ]
+
+    for col in categorical_columns:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        encoders[col] = le
+
+    target_encoder = LabelEncoder()
+    df["Sleep_disorder"] = target_encoder.fit_transform(
+        df["Sleep_disorder"]
+    )
+
+    X = df.drop("Sleep_disorder", axis=1)
+    y = df["Sleep_disorder"]
+
+    return X, y, encoders, target_encoder
